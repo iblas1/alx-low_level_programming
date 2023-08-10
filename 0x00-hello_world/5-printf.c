a8995320df91d46faf399cc5673d21fb217a33ae
iblas <ibrahimiblas@gmail.com>
2023-08-10 23:11:49 +0100

    c programming commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+*main - Program entry point
+*print something out
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
