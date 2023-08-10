22d4c96da5d27dfbcf9552fd9baa9a41cd0dc9aa
iblas <ibrahimiblas@gmail.com>
2023-08-10 23:19:47 +0100

    c programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
-/* print something */
-
+/**
+*main - Program entry point
+*print something to the monitor
+*
+*Return: Always 0 (Sucess)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
