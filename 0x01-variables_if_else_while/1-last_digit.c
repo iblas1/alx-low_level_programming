5d8d00083a14a85705b703e1a1f15d4497abf1e3
iblas <ibrahimiblas@gmail.com>
2023-08-13 11:26:22 +0100

    conditionals in c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - entry point
+*function that assign random number
+*
+*Return: Always 0 (Sucess)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int lastDigit = n%10;
+	if (lastDigit > 5)
+	printf("Last digit of %i is %i and is greater than 5\n", n, lastDigit);
+	else if (lastDigit == 0)
+	printf("Last digit of %i is %i and is 0\n", n, lastDigit);
+	else if (lastDigit < 6)
+	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastDigit);
+	return (0);
+}
