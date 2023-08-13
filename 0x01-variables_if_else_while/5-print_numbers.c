f88b4f92f3fc8ce602dca713adbb3929c21b16b7
iblas <ibrahimiblas@gmail.com>
2023-08-13 15:22:10 +0100

    conditionals in c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - entry point
+*function that print small letter except q and e
+*
+*Return: Always 0 (Sucess)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+n = 0;
+while (n < 10)
+{
+printf("%i", n);
+n++;
+}
+printf("\n");
+return (0);
+}
