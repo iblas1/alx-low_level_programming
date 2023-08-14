ce7b4433554bf926b5ab8d0d801fc1abc5bdc892
iblas <ibrahimiblas@gmail.com>
2023-08-14 17:57:49 +0100

    back to hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
+#include <stdio.h>
 /**
 * main - entry point
 * display a particular message to stand error
 *
 * Return: Sucess (1)
 */
-int main()
+int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 62);
-	return 1;
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
 
