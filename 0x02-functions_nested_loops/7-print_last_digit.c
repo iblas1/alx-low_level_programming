590abab9d1e8162fac5015a4aa2d3ea1a97e83f2
iblas <ibrahimiblas@gmail.com>
2023-08-16 22:04:47 +0100

    functions in c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - print a sign of a number
+ * @num: the number to print its sign
+ *
+ * Return: Success (0)
+ */
+
+int print_last_digit(int num)
+{
+	int lastDigit;
+
+	lastDigit = (num % 10);
+	_putchar(lastDigit + '0');
+	_putchar('\n');
+	return (0);
+}
