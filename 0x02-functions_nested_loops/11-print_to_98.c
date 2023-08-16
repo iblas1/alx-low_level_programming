fd6fef247bae9f4fcc292e2c3b3d59f4675602ed
iblas <ibrahimiblas@gmail.com>
2023-08-16 23:25:55 +0100

    functions in c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,27 @@
 
 void print_to_98(int n)
 {
+if (n <= 98)
+{
 	for (n = n; n <= 98; n++)
 	{
-	if (n != 98)
-	printf("%i, ", n);
-	else
-	printf("%i\n", n);
+		if (n != 98)
+		printf("%i, ", n);
+		else
+		printf("%i\n", n);
 	}
+}
+if (n > 98)
+{
+
+	for (n = n; n >= 98; n--)
+	{
+		if (n != 98)
+		printf("%i, ", n);
+		else
+		printf("%i\n", n);
+	}
+}
 }
 
 
