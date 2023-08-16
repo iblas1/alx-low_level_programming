ae4b62a558028562770bc749c1e7dc3a71231afd
iblas <ibrahimiblas@gmail.com>
2023-08-16 23:16:08 +0100

    functions in c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - print a sign of a number
+ *@n: initial number
+ * Return: Success (0)
+ */
+
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+	{
+	if (n != 98)
+	printf("%i, ", n);
+	else
+	printf("%i\n", n);
+	}
+}
+
+
