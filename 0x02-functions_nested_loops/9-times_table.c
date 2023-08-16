7cf6336404b4d9ea048daeb3a6ff54206265f244
iblas <ibrahimiblas@gmail.com>
2023-08-16 23:56:36 +0100

    functions in c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *times_table - print a sign of a number
+ * Return: Success (0)
+ */
+
+void times_table(void)
+{
+int row, col;
+
+for (row = 0; row <= 9; row++)
+{
+	for (col = 0; col <= 9; col++)
+	{
+	printf("%2d, ", row * col);
+	}
+	putchar('\n');
+}
+}
