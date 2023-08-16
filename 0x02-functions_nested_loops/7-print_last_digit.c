e94c5d744c9c40f0fe785157f99dbf19f74bdbee
iblas <ibrahimiblas@gmail.com>
2023-08-17 00:29:46 +0100

    functions in c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,22 +11,12 @@ int print_last_digit(int num)
 {
 	int lastDigit;
 
-	if (num < 0)
-	lastDigit = (num * -1) % 10;
-	else
 	lastDigit = (num % 10);
+
+	if (lastDigit < 0)
+	lastDigit = (lastDigit * -1);
+
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
 
-int main(void)
-{
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
-}
