1f89c1f05df52aac15c3d97fbc3b02188a62c98c
iblas <ibrahimiblas@gmail.com>
2023-08-18 00:23:32 +0100

    more nested loop functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,24 +2,30 @@
 #include <stdio.h>
 
 /**
-* fizz_buzz - print straight lines
+* main - print fizzbuzz
+*
+* Return: Success (0)
 */
 
-void fizz_buzz(void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 		else if (i % 5 == 0)
-		printf("Buzz ");
+		printf("Buzz");
 		else if (i % 3 == 0)
-		printf("Fizz ");
+		printf("Fizz");
 		else
 		printf("%d ", i);
 
+		if (i != 100)
+		printf(" ");
+
 	}
 	printf("\n");
+	return (0);
 }
