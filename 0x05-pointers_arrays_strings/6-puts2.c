94c4c540cae4b735e90cd9db20e6fff2a9fa9404
iblas <ibrahimiblas@gmail.com>
2023-08-22 23:45:34 +0100

    arrays, pointers and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+* _strlen - return the length of a string
+* @s: string
+* Return: Sucess (0)
+*/
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+
+/**
 * puts2 - print every other chracter
 * @str: string
 */
