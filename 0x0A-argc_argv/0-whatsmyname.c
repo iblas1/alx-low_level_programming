ceb0a3cf0183968691ce2cba1cc81df960e732d1
iblas <ibrahimiblas@gmail.com>
2023-09-03 15:47:57 +0100

    argument to main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,10 +12,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s ", argv[i]);
-	}
-	printf("\n");
+	if (argc > 0)
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
