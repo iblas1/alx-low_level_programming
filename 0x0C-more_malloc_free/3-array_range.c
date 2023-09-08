cdffead36171476afe6ef3fba610aa7201a5382e
iblas <ibrahimiblas@gmail.com>
2023-09-08 20:31:25 +0100

    malloc stuff

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* _calloc - to concatenate a string to an allocated memory
+* @nmemb: destination string
+* @size: source string
+* Return: pointer to the array created
+*/
+
+int *array_range(int min, int max)
+{
+	int *array, i, j = 0, element_size = 0;
+
+	if (min > max)
+	return NULL;
+
+	element_size = (max - min) + 1;
+	
+	array = malloc(sizeof(int) * element_size);
+	if (array == NULL)
+	return NULL;
+	
+	i = 0;
+	for (j = min; j <= max; j++, i++)
+	array[i] = j;
+	
+	return (array);	
+}
